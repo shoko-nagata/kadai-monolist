38bbe86a0fea81f52b8269550a0aa765a628f694
shoko.nagata <shoko.nagata@rakuten.com>
2018-06-22 03:01:16 +0000

    want

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -1,29 +1,64 @@
-use \App\Item;
+<?php
 
-  class ItemsController extends Controller
-  {
+namespace App\Http\Controllers;
 
-    public function create()
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
+
+use App\Item;
+
+class ItemUserController extends Controller
+{
+    public function want()
     {
-        $keyword = request()->keyword;
-        $items = [];
-        if ($keyword) {
-            $client = new \RakutenRws_Client();
-            $client->setApplicationId(env('RAKUTEN_APPLICATION_ID'));
+        $itemCode = request()->itemCode;
 
-            $rws_response = $client->execute('IchibaItemSearch', [
-                'keyword' => $keyword,
-                'imageFlag' => 1,
-                'hits' => 20,
-            ]);
+        // Search items from "itemCode"
+        $client = new \RakutenRws_Client();
+        $client->setApplicationId(env('RAKUTEN_APPLICATION_ID'));
+        $rws_response = $client->execute('IchibaItemSearch', [
+            'itemCode' => $itemCode,
+        ]);
+        $rws_item = $rws_response->getData()['Items'][0]['Item'];
 
-             // Creating "Item" instance to make it easy to handle.（not saving）
-            foreach ($rws_response->getData()['Items'] as $rws_item) {
-                $item = new \App\Item();
-                $item->code = $rws_item['Item']['itemCode'];
-                $item->name = $rws_item['Item']['itemName'];
-                $item->url = $rws_item['Item']['itemUrl'];
-                $item->image_url = str_replace('?_ex=128x128', '', $rws_item['Item']['mediumImageUrls'][0]['imageUrl']);
-                $items[] = $item;
-            }
-  }
+        // create Item, or get Item if an item is found
+        $item = Item::firstOrCreate([
+            'code' => $rws_item['itemCode'],
+            'name' => $rws_item['itemName'],
+            'url' => $rws_item['itemUrl'],
+            // remove "?_ex=128x128" because its size is defined
+            'image_url' => str_replace('?_ex=128x128', '', $rws_item['mediumImageUrls'][0]['imageUrl']),
+        ]);
+
+        \Auth::user()->want($item->id);
+
+        return redirect()->back();
+    }
+
+    public function dont_want()
+    {
+        $itemCode = request()->itemCode;
+
+        if (\Auth::user()->is_wanting($itemCode)) {
+            $itemId = Item::where('code', $itemCode)->first()->id;
+            \Auth::user()->dont_want($itemId);
+        }
+        return redirect()->back();
+    }
+    
+    
+    public function show($id)
+    {
+      $item = Item::find($id);
+      $want_users = $item->want_users;
+
+      return view('items.show', [
+          'item' => $item,
+          'want_users' => $want_users,
+      ]);
+    }
+    
+    
+    
+}
