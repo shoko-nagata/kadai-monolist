03e9c2d4ece4d5cb4342b606a5210ca8cae2083c
shoko.nagata <shoko.nagata@rakuten.com>
2018-06-25 01:54:51 +0000

    have ranking

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,9 +1,23 @@
-@if (Auth::check())
+<header>
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-left" href="/"><img src="{{ secure_asset("images/logo.png") }}" alt="Monolist"></a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                   @if (Auth::check())
                         <li>
-                            <a href="#">
+                           <a href="{{ route('items.create') }}">
                                 <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
                                 アイテムを追加
-                              </a>
+                            </a>
                         </li>
 
                         <li class="dropdown">
@@ -15,7 +29,6 @@
                             <ul class="dropdown-menu">
                                 <li><a href="{{ route('ranking.want') }}">Wantランキング</a></li>
                                  <li><a href="{{ route('ranking.have') }}">Haveランキング</a></li>
-                            </ul>
                             </ul>
                         </li>
 
@@ -38,3 +51,11 @@
                             </ul>
                         </li>
                     @else
+                        <li><a href="{{ route('signup.get') }}">新規登録</a></li>
+                        <li><a href="{{ route('login') }}">ログイン</a></li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
